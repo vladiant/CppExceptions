294cda6fc8714d80e32033bf5558327c3ec7488f
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-01-06 00:10:45 +0200

    Add exceptional control flow same operation sample

---
+++ exceptional_control_flow_same_operation/exceptional_control_flow_same_operation.cpp
@@ -0,0 +1,61 @@
+#include <expected>
+#include <iostream>
+#include <source_location>
+#include <string_view>
+
+struct Info {
+};
+struct Msg {
+};
+
+struct ExceptionOrderNotFound : std::runtime_error {
+  using std::runtime_error::runtime_error;
+  Msg msg_;
+};
+struct ExceptionIllegalCurrency : std::runtime_error {
+  using std::runtime_error::runtime_error;
+  Msg msg_;
+};
+struct ExceptionInvalidClientId : std::runtime_error {
+  using std::runtime_error::runtime_error;
+  Msg msg_;
+};
+
+bool send(const Msg& request) {
+  (void) request;
+  return true;
+}
+
+Msg apply(const Info& data) {
+  (void) data;
+  throw ExceptionOrderNotFound{"illegal format"};
+  return Msg{};
+}
+
+bool process(const Info& data) {
+    try {
+      Msg request = apply(data);
+      return send(request);
+    }
+    catch(ExceptionOrderNotFound& e) {
+      return send(e.msg_);
+    }
+    catch(ExceptionIllegalCurrency& e) {
+      return send(e.msg_);
+    }
+    catch(ExceptionInvalidClientId& e) {
+      return send(e.msg_);
+    }
+    return true;
+}
+
+int main() {
+    try {
+      process(Info{});
+    }
+    catch(...) {
+        std::cout << "Unknown exception" << std::endl;
+    }
+    std::cout << "End" << std::endl;
+    return 0;
+}
